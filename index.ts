3577702d7c0efa4330f1f07758019a051b6b853a
Patrik Csak <patrik@csak.email>
2018-06-16 18:07:36 -0700

    Add Arkansas

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import alabama from './states/alabama'
 import alaska from './states/alaska'
 import arizona from './states/arizona'
+import arkansas from './states/arkansas'
 
 class Lpsg {
     public static alabama = alabama
     public static alaska = alaska
     public static arizona = arizona
+    public static arkansas = arkansas
 }
 
 export default Lpsg
