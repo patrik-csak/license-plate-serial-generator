18083b8a6e6ce41f0a7ed3714d56aad439e4eec7
Patrik Csak <patrik@csak.email>
2018-06-16 17:56:55 -0700

    Add index.ts

---
+++ index.ts
@@ -0,0 +1,11 @@
+import alabama from './states/alabama'
+import alaska from './states/alaska'
+import arizona from './states/arizona'
+
+class Lpsg {
+    public static alabama = alabama
+    public static alaska = alaska
+    public static arizona = arizona
+}
+
+export default Lpsg
